48c89306b90b038b9764f8cda8201fddd5f2690b
Jerry Seyi <jerryseyi68@gmail.com>
2024-03-16 09:38:45 +0100

    register and login controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (! Auth::attempt($data)) {
+            return response(['message' => 'Invalid Credentials']);
+        }
+
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+        return response(['user' => Auth::user(), 'access_token' => $accessToken]);
+    }
+
+    public function register(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
+        ]);
+
+        $data['password'] = bcrypt($request->password);
+        $user = User::create($data);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'access_token' => $accessToken]);
+    }
+}
